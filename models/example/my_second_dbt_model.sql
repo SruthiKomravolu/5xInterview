cfd1f8928ad9707b1ae8e7a01950ca497fcaf978
sruthikv236@gmail.com <sruthikv236@gmail.com>
2021-11-28 14:54:40 +0000

    yes

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -5,3 +5,15 @@
 from {{ ref('my_first_dbt_model') }}
 where id = 1
 */
+create or replace  view INTERVIEW_DB.PLAYGROUND_venkatasruthi_komaravolu.weekbyweek  as (
+select WEEK(to_date(date, 'MM/DD/YYYY')) as week, year(to_date(date, 'MM/DD/YYYY')) as year, 
+            concat(WEEK(to_date(date, 'MM/DD/YYYY')),'_ ', year(to_date(date, 'MM/DD/YYYY'))) as week_year,
+            INDONESIA,COUNTRY,
+              max(TOTAL_CASES) as Total_cases ,sum(new_cases) as New_Positive_cases
+
+from 
+
+FIVETRAN_INTERVIEW_DB.GOOGLE_SHEETS.COVID_19_INDONESIA_VENKATASRUTHI_KOMARAVOLU group by INDONESIA, week_year,week,year, COUNTRY 
+order by year,week
+
+)
