fb25a69621bb4d9e369aec1b526360703ed7818d
sruthikv236@gmail.com <sruthikv236@gmail.com>
2021-12-01 12:05:56 +0000

    final

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -6,7 +6,38 @@
 
     Try changing "table" to "view" below
 */
+create or replace  view INTERVIEW_DB.PLAYGROUND_venkatasruthi_komaravolu.death_rate  as (
+select INDONESIA,(max(TOTAL_DEATHS)/max(population)*100) as rate from FIVETRAN_INTERVIEW_DB.GOOGLE_SHEETS.COVID_19_INDONESIA_VENKATASRUTHI_KOMARAVOLU
+group by INDONESIA order by rate desc)
 
+
+create or replace  view INTERVIEW_DB.PLAYGROUND_venkatasruthi_komaravolu.monthbymonth  as (
+select month(to_date(date, 'MM/DD/YYYY')) as month, year(to_date(date, 'MM/DD/YYYY')) as year, 
+            concat(month(to_date(date, 'MM/DD/YYYY')),'_ ', year(to_date(date, 'MM/DD/YYYY'))) as month_year,
+            INDONESIA,COUNTRY,
+              max(TOTAL_CASES) as Total_cases ,sum(new_cases) as New_Positive_cases
+
+from 
+
+FIVETRAN_INTERVIEW_DB.GOOGLE_SHEETS.COVID_19_INDONESIA_VENKATASRUTHI_KOMARAVOLU group by INDONESIA, month_year,month,year, COUNTRY 
+order by year,month
+
+)
+
+
+
+create or replace  view INTERVIEW_DB.PLAYGROUND_venkatasruthi_komaravolu.weekbyweek  as (
+select WEEK(to_date(date, 'MM/DD/YYYY')) as week, year(to_date(date, 'MM/DD/YYYY')) as year, 
+            concat(WEEK(to_date(date, 'MM/DD/YYYY')),'_ ', year(to_date(date, 'MM/DD/YYYY'))) as week_year,
+            INDONESIA,COUNTRY,
+              max(TOTAL_CASES) as Total_cases ,sum(new_cases) as New_Positive_cases
+
+from 
+
+FIVETRAN_INTERVIEW_DB.GOOGLE_SHEETS.COVID_19_INDONESIA_VENKATASRUTHI_KOMARAVOLU group by INDONESIA, week_year,week,year, COUNTRY 
+order by year,week
+
+)
 
 
 --{{ config(materialized='view') }}
@@ -21,6 +52,9 @@
 */
 --select *
 --from source_data
+
+
+
 
 
 
